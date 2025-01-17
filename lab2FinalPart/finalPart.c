4a94a828f4f1d63b83a6acf18f44e8ef5b638c25
Alec Mirambeau <alecmirambeau@gmail.com>
2025-01-17 10:44:58 -0500

    bug where loop never terminates

---
+++ lab2FinalPart/finalPart.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[]) {
+    char userInput[40];
+    while (1){
+        if (!strcmp (userInput, "quit")){
+            printf("terminating program.\n");
+            break;
+        }
+        fgets(userInput, 40, stdin);
+        printf("string: %s\n", userInput);
+
+    }
+//     if (argc < 3) {
+//         fputs("Usage: must supply a command\n", stderr);
+//         exit(1);
+//     }
+//     puts("Before the exec");
+//     if (execvp(argv[1], &argv[1]) < 0) {
+//         perror("exec failed");
+//         exit(1);
+//     }
+//     puts("After the exec");
+    return 0;
+ }
