9c0380ee1692543156774823eb47c2755f1fbdf0
Alec Mirambeau <alecmirambeau@gmail.com>
2025-01-25 21:22:14 -0500

    started lab 3

--- lab3ProgrammingPart/program.c
+++ lab3ProgrammingPart/program.c
@@ -1,1 +1,21 @@
 // this is our program
+#include <stdio.h>
+#include <unistd.h>
+int main(int argc, char *argv[]){
+
+    int pid;
+    pid = fork();
+
+    if (pid == 0){
+        // in child process.
+    } else {
+        // in parent process.
+        printf("Spawned child PID# %d\n", pid);
+    }
+
+    return 0;
+}
+
+void SIGUSR1(){
+
+}
