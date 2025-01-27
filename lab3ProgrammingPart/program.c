ca39f35bbe4dfdfa8802eff388b87f27a9a8d600
Connor Ostrowski <ostrocon@mail.gvsu.edu>
2025-01-27 15:30:35 -0500

    work
    mostly done

--- lab3ProgrammingPart/program.c
+++ lab3ProgrammingPart/program.c
@@ -1,54 +1,69 @@
 // this is our program
 #include <stdio.h>
 #include <unistd.h>
-#include <signal.h> 
+#include <signal.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]){
+// Signal handlers
+void handle_sigusr1(int sig) {
+    printf("Signal received was: SIGUSR1\n");
+}
 
+void handle_sigusr2(int sig) {
+    printf("Signal received was: SIGUSR2\n");
+}
+
+void handle_cntrl_c(int sig) {
+    printf("Signal received was Control + C\n");
+    printf("Time to exit I'm shutting down...\n");
+    exit(0);
+}
+
+int main(int argc, char *argv[]) {
     int pid;
+
+    // Fork a child process
     pid = fork();
 
-    if (pid == 0){
-        // in child process.
-        // want to continue randomly choosing a signal so will just do an infinite loop
-        while (1){
-            // get a random time 0 to 5
+    if (pid == 0) {
+        // In child process
+        while (1) {
+            // Get a random time 0 to 5 seconds
             int timeToWait = rand() % 6;
-            // Wait the randomly specified amount of time.
-            sleep(timeToWait);
-            // Can just do if statement if 1 send sig usr 1 else send usr sig 2.
-            int signalToSend = rand() % 2;
-        }
-    } else {
-        // in parent process.
-        // print out child pid
+            sleep(timeToWait); // Wait for the randomly specified amount of time
+
+            // Randomly choose a signal to send
+            int signalToSend = rand() % 2 == 0 ? SIGUSR1 : SIGUSR2;
+            // Send the signal to the parent process
+            kill(getppid(), signalToSend); 
+        }     
+    } 
+    
+    else {
+        // In parent process
+        // Print out child PID
         printf("Spawned child PID# %d\n", pid);
-        // ensure SIGUSR1 handler is setup successfully.
-        if(signal(SIGUSR1, handle_sigusr1) == SIG_ERR){
+
+        // Set up the SIGUSR1 handler
+        if (signal(SIGUSR1, handle_sigusr1) == SIG_ERR) {
             perror("Error setting up signal handler for SIGUSR1");
         }
-        // ensure SIGUSR2 handler is setup successfully.
-                if(signal(SIGUSR2, handle_sigusr2) == SIG_ERR){
+
+        // Set up the SIGUSR2 handler
+        if (signal(SIGUSR2, handle_sigusr2) == SIG_ERR) {
             perror("Error setting up signal handler for SIGUSR2");
         }
 
+        // Set up the SIGINT (Control+C) handler
+        if (signal(SIGINT, handle_cntrl_c) == SIG_ERR) {
+            perror("Error setting up signal handler for SIGINT");
+        }
 
+        // Parent process waits indefinitely for signals
+        while (1) {
+            pause(); // Wait for a signal to arrive
+        }
     }
 
     return 0;
-}
-
-void handle_sigusr1(int sig){
-    printf("Signal recieved was: SIGUSR1\n");
-}
-
-void handle_sigusr2(int sig){
-    printf("Signal recieved was: SIGUSR2\n");
-}
-
-void handle_cntrl_c(int sig){
-    printf("Signal received was control + c");
-    printf("time to exit after doing cleanup work");
-    exit(0);
 }
