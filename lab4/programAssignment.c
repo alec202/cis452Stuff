1ef5d696097720f3955e8b4e51f85bada03557a6
Alec Mirambeau <alecmirambeau@gmail.com>
2025-02-01 18:14:08 -0500

    almost done, just need to fix bug causing duplicate file success read statements

--- lab4/programAssignment.c
+++ lab4/programAssignment.c
@@ -1,31 +1,80 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 // shared storage for keeping track of the number of files accessed like the lab asks.
 int fileRequestsReceived = 0;
-// assume there's some upper bound, we can choose what that will be. In our case we can do 100 just to hope it doesn't wait a super long time.
+pthread_t thread1ID;
+void* executionThread(void* fileNamePassedToFunction);
+
+// setup control c handler.
+void handle_cntrl_c(int sig) {
+    printf("\nSignal received was Control + C\n");
+    printf("Total file requests received was %d.\n", fileRequestsReceived);
+    void* result1;
+    int joinStatus;
+    joinStatus = pthread_join(thread1ID, &result1);
+    if (joinStatus != 0) {
+        fprintf (stderr, "Join error %d: %s\n", joinStatus, strerror(joinStatus));
+        }
+    exit(0);
+}
+
 int main(){
     int threadCreationSuccess;
-    pthread_t thread1ID;
     char fileToAccess[20];
     puts("This is a program to simulate file accesses.");
-    while (strcmp(fileToAccess, "quit\n") != 0){
+    // Set up the SIGINT (Control+C) handler
+    if (signal(SIGINT, handle_cntrl_c) == SIG_ERR) {
+        perror("Error setting up signal handler for SIGINT");
+    }
+    // loop to simulate file access.
+    while (strcmp(fileToAccess, "quit") != 0){
         // get name of file
         puts("Enter the name of the file you want to access or enter 'quit' to stop.");
         fgets(fileToAccess, 20, stdin);
-        if (strcmp(fileToAccess, "quit\n") == 0){
+        //Remove trailing newline character, \n , from fgets
+        for (int i = 0; fileToAccess[i] != '\0'; i++) 
+        {
+    		if (fileToAccess[i] == '\n') 
+    		{
+        		fileToAccess[i] = '\0';
+        		break; // Exit loop once newline is replaced
+    		}
+	    }
+        if (strcmp(fileToAccess, "quit") == 0){
             break;
         }
+
         fileRequestsReceived++;
         // now that we know the name of the file we want to access let's make the child thread.
-        // threadCreationSuccess = pthread_create(&thread1ID, NULL, executionThread, fileToAccess);
+        threadCreationSuccess = pthread_create(&thread1ID, NULL, executionThread, fileToAccess);
+        // make sure the thread was created successfully.
+        if (threadCreationSuccess != 0){
+            fprintf (stderr, "thread create error %d: %s\n", threadCreationSuccess, strerror(threadCreationSuccess));
+        }
     }
 
     puts("End of program, goodbye.");
     return 0;
 }
 
-void executionThread(void* fileNamePassedToFunction){
-
+void* executionThread(void* fileNamePassedToFunction){
+    // use this variable below instead of fileNamePassedToFunction as it's been casted to a string whereas fileNamePassedToFunction is of type void *.
+    char* fileName = (char *)fileNamePassedToFunction;
+    // get a random number in range 1 - 10: https://www.geeksforgeeks.org/generating-random-number-range-c/
+    int randNum = rand() % (10) + 1;
+    // 20% of the time, if the file is in the hdd v
+    if (randNum < 3){
+        // sleep for a random amount of time from 7 - 10 seconds
+        int randSleepAmount = rand() % (4) + 7;
+        sleep(randSleepAmount);
+    // the file was found in the cache so we sleep for only 1 second.
+    } else {
+        sleep(1);
+    }
+    printf("File %s has been accessed successfully.\n", fileName);
+    return NULL;
 }
