b320fc584ec51ffb28ad9635f819f09e05546fec
Alec Mirambeau <alecmirambeau@gmail.com>
2025-04-02 16:18:55 -0400

    actually finished for real

--- lab12/programAssignment.c
+++ lab12/programAssignment.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <errno.h>
 #include <dirent.h>
+#include <errno.h>
+#include <string.h>
 
 
 int main(int argc, char *argv[])
@@ -21,13 +22,23 @@ int main(int argc, char *argv[])
     // opens directory stream for specified directory.
     char* directoryName = argv[1];
     dirPtr = opendir(directoryName);
+    if (dirPtr == NULL) {
+        // opendir failed – use errno to determine why
+        fprintf(stderr, "Failed to open directory '%s': %s\n", directoryName, strerror(errno));
+        return EXIT_FAILURE;
+    }
 
+    printf("specified directory is: %s\n", directoryName);
     while ((entryPtr = readdir(dirPtr))){
-        if (stat(entryPtr->d_name, &fileStatBuff) < 0){
+        // Create full path: directoryName + "/" + entry name
+        char fullPath[1024];
+        snprintf(fullPath, sizeof(fullPath), "%s/%s", directoryName, entryPtr->d_name);
+        if (stat(fullPath, &fileStatBuff) < 0){
+            printf("in directory: %s\n", fullPath);
             perror("Program error with the stat command:  ");
             exit(1);
         } 
-        printf("%-20s\tThe user ID is: %u, the group ID is: %u. The inode value is: %llu\n", entryPtr->d_name, fileStatBuff.st_uid, fileStatBuff.st_gid, fileStatBuff.st_ino);
+        printf("%-20s\tThe user ID is: %u, the group ID is: %u. The inode value is: %llu\n", fullPath, fileStatBuff.st_uid, fileStatBuff.st_gid, fileStatBuff.st_ino);
     }
     closedir(dirPtr);    
     return 0;
